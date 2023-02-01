a2b3d4259c308850b54f05647f997d9be5e43403
Myles Reyburn <mylesreyburn@gmail.com>
2023-02-01 15:15:41 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names){
+    let cards = [];
+    for(let i = 0; i < names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    return cards;
+}
+
+function countDown(i){
+    while(i >= 0){
+        console.log(i);
+        i--;
+    }
+}
